d4207da0d331ae6224a826f3f73bfab80e000eae
Lior <liorcohen2156@gmail.com>
2026-07-29 20:15:31 +0300

    Add the start

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require('../controllers/authControllers');
 
 router.post('/login', authController.login);
 router.get('/index2.html', authController.protectFeed);
